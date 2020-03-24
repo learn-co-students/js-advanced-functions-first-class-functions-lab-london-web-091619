bbcc1199bc6835f3ad00a8446bc828ec7fc711ad
Michael Williamson <michaelrwilliamson92@gmail.com>
2020-03-24 13:08:38 +0000

    added other functions

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const returnFirstTwoDrivers = function (drivers) {
   const returnLastTwoDrivers = function (drivers) {
     return drivers.slice(-2);
   };
+
+  const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+
+const createFareMultiplier = function (multiplyValue) {
+  return function (value) {
+    return multiplyValue * value;
+  };
+};
